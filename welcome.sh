e63dc03f032904ea6651310db1ee0a0ba7a4d5b8
stainedpants <mikewhelan10@gmail.com>
2023-01-28 15:57:35 -0500

    initial

---
+++ welcome.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+sudo apt install figlet  # version 2.2.5-3
+figlet Happy Incredible Valentines! by big mike
+
+cat > test.html << EOF
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>New Page</title>
+    </head>
+    <body>
+        <h1>Hello, World!</h1>
+    </body>
+</html>
+
+EOF
+
+
